d8534f4cf2d449639af2f9f6c342371c1b9ad978
om mapari <mapariom05@gmail.com>
2023-02-11 17:42:29 +0530

    classes separated

---
+++ gest.py
@@ -0,0 +1,25 @@
+# Gesture Encodings 
+from enum import IntEnum
+
+class Gest(IntEnum):
+    # Binary Encoded
+    """
+    Enum for mapping all hand gesture to binary number.
+    """
+
+    FIST = 0
+    PINKY = 1
+    RING = 2
+    MID = 4
+    LAST3 = 7
+    INDEX = 8
+    FIRST2 = 12
+    LAST4 = 15
+    THUMB = 16    
+    PALM = 31
+    
+    # Extra Mappings
+    V_GEST = 33
+    TWO_FINGER_CLOSED = 34
+    PINCH_MAJOR = 35
+    PINCH_MINOR = 36
---
+++ hlabel.py
@@ -0,0 +1,7 @@
+from enum import IntEnum
+
+
+# Multi-handedness Labels
+class HLabel(IntEnum):
+    MINOR = 0
+    MAJOR = 1
